97bca0a84a034a192317610f933a15e498fb0eff
Vikram <vikram7sarun@gmail.com>
2020-07-27 20:32:25 +0530

    1.pip install. 2.pytest install. 3.requirements file implement. 4.sample test create

---
+++ test/test_trailer.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+class TestTrailer:
+
+    def test_one(self):
+        print("This is test 1")
+
+    def test_two(self):
+        print("This is test 2")
+
+    def test_three(self):
+        print("This is test 3")
+
+
